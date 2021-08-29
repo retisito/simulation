8fb36a26a95eacf32718a429c2759e6294b40947
Adrian Chacon <retisito@gmail.com>
2021-08-29 14:58:45 -0400

    Work in progress

--- process.py
+++ process.py
@@ -4,7 +4,12 @@ import sys
 
 class Process:
 
-  def __init__(self, receive, send, resource):
+  def __init__(self, receive, send, id = 0):
+    self.id = id
     self.receive = receive
     self.send = send
-    self.resource = resource
+    #self.resource = resource
+
+if __name__ == "__main__":
+  p1 = Process(Server(port = 9000), Client(port = 9001), 0)
+  p2 = Process(Server(port = 9001), Client(port = 9000), 1)
